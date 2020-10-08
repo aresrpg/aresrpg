c46453a918724844a1d29365ea64f875c300490e
Mathis <mathis.paul.62@gmail.com>
2020-10-08 23:44:14 +0200

    added : trade window opening and mob spawn test

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import update_chunks from './chunk/update.js'
 import EventEmitter from 'events'
 import { position_change_event } from './events.js'
 import { chunk_change_event, chunk_position } from './chunk.js'
+import { worldMobsSpawn } from './worldMobsSpawn/worldMobsSpawn.js'
+import { openTrade } from './trade/trade.js'
 
 const server = protocol.createServer({ version, 'online-mode': online_mode })
 
@@ -21,11 +23,13 @@ server.on('login', (client) => {
     events: new EventEmitter(),
     gameMode: 1,
   }
-
+  client.on('end', console.error)
   login(state)
   position_change_event(state)
   chunk_change_event(state)
   update_chunks(state)
+  worldMobsSpawn(state)
+  openTrade(state)
 })
 
 server.on('listening', () => {
