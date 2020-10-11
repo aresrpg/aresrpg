fb0bfe2d2ddb4a0f33028f53a35f220db198de2a
Duarte David <deltaduartedavid@gmail.com>
2020-10-11 21:28:58 +0200

    Add a world state to handle global ids

--- src/trade/spawn_villager.js
+++ src/trade/spawn_villager.js
@@ -4,21 +4,34 @@ import minecraftData from 'minecraft-data'
 
 const mcData = minecraftData(version)
 
-export function spawn_villager({ client }) {
-  const { id: type } = mcData.entitiesByName['villager']
-  const mob = {
-    entityId: 69420,
-    entityUUID: UUID.v4(),
-    type,
-    x: 469,
-    y: 163,
-    z: 649,
-    yaw: 0,
-    pitch: 0,
-    headPitch: 0,
-    velocityX: 0,
-    velocityY: 0,
-    velocityZ: 0,
+export function spawn_villager(world) {
+  const entityId = world.lastEntityId
+
+  function spawn_villager_handler({ client }) {
+    const { id: type } = mcData.entitiesByName['villager']
+    const mob = {
+      entityId,
+      entityUUID: UUID.v4(),
+      type,
+      x: 469,
+      y: 163,
+      z: 649,
+      yaw: 0,
+      pitch: 0,
+      headPitch: 0,
+      velocityX: 0,
+      velocityY: 0,
+      velocityZ: 0,
+    }
+    client.write('spawn_entity_living', mob)
   }
-  client.write('spawn_entity_living', mob)
+
+  return {
+    handlers: [spawn_villager_handler],
+    world: {
+      ...world,
+      lastEntityId: world.lastEntityId + 1,
+      villagers: [entityId],
+    },
+  }
 }
