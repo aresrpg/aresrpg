46f0c74511cdda31ade9d0451d40a135405c196f
Duarte David <deltaduartedavid@gmail.com>
2020-10-28 22:55:14 +0100

    Simplify code by removing handler list

--- src/mobs/spawn_mob.js
+++ src/mobs/spawn_mob.js
@@ -15,7 +15,7 @@ const color_by_type = {
 
 const chunk_index = (x, z) => `${x}:${z}`
 
-export function spawn_mob(world) {
+export function register_mobs(world) {
   //generate an object containing mobs by chunk
   const mobs_in_chunk = world.mobs.reduce(
     (map, { position, mob: mob_name, level }, i) => {
@@ -42,73 +42,75 @@ export function spawn_mob(world) {
     new Map()
   )
 
-  function spawn_mob_handler({ client, events }) {
-    events.on('chunk_loaded', ({ x, z }) => {
-      if (mobs_in_chunk.has(chunk_index(x, z))) {
-        for (const {
-          id,
-          mob: minecraft_mob,
-          displayName,
-          type,
-          level,
-          position,
-        } of mobs_in_chunk.get(chunk_index(x, z))) {
-          const mob = {
-            entityId: id,
-            entityUUID: UUID.v4(),
-            type: mcData.entitiesByName[minecraft_mob].id,
-            x: position.x,
-            y: position.y,
-            z: position.z,
-            yaw: 0,
-            pitch: 0,
-            headPitch: 0,
-            velocityX: 400,
-            velocityY: 400,
-            velocityZ: 400,
-          }
-
-          const metadata = {
-            entityId: id,
-            metadata: [
-              {
-                key: 2,
-                value: JSON.stringify({
-                  text: displayName,
-                  color: color_by_type[type],
-                  extra: level && [
-                    { text: ` [Lvl ${level}]`, color: 'dark_red' },
-                  ],
-                }),
-                type: 5,
-              },
-              {
-                key: 3,
-                type: 7,
-                value: true,
-              },
-            ],
-          }
-
-          client.write('spawn_entity_living', mob)
-          client.write('entity_metadata', metadata)
-        }
-      }
-    })
-
-    events.on('chunk_unloaded', ({ x, z }) => {
-      if (mobs_in_chunk.has(chunk_index(x, z))) {
-        client.write('entity_destroy', {
-          entityIds: mobs_in_chunk.get(chunk_index(x, z)).map(({ id }) => id),
-        })
-      }
-    })
-  }
   return {
-    handlers: [spawn_mob_handler],
-    world: {
-      ...world,
-      lastEntityId: world.lastEntityId + world.mobs.length,
+    ...world,
+    lastEntityId: world.lastEntityId + world.mobs.length,
+    mobs: {
+      mobs_in_chunk,
     },
   }
+}
+
+export function spawn_mob({ client, events, world }) {
+  const { mobs_in_chunk } = world.mobs
+  events.on('chunk_loaded', ({ x, z }) => {
+    if (mobs_in_chunk.has(chunk_index(x, z))) {
+      for (const {
+        id,
+        mob: minecraft_mob,
+        displayName,
+        type,
+        level,
+        position,
+      } of mobs_in_chunk.get(chunk_index(x, z))) {
+        const mob = {
+          entityId: id,
+          entityUUID: UUID.v4(),
+          type: mcData.entitiesByName[minecraft_mob].id,
+          x: position.x,
+          y: position.y,
+          z: position.z,
+          yaw: 0,
+          pitch: 0,
+          headPitch: 0,
+          velocityX: 400,
+          velocityY: 400,
+          velocityZ: 400,
+        }
+
+        const metadata = {
+          entityId: id,
+          metadata: [
+            {
+              key: 2,
+              value: JSON.stringify({
+                text: displayName,
+                color: color_by_type[type],
+                extra: level && [
+                  { text: ` [Lvl ${level}]`, color: 'dark_red' },
+                ],
+              }),
+              type: 5,
+            },
+            {
+              key: 3,
+              type: 7,
+              value: true,
+            },
+          ],
+        }
+
+        client.write('spawn_entity_living', mob)
+        client.write('entity_metadata', metadata)
+      }
+    }
+  })
+
+  events.on('chunk_unloaded', ({ x, z }) => {
+    if (mobs_in_chunk.has(chunk_index(x, z))) {
+      client.write('entity_destroy', {
+        entityIds: mobs_in_chunk.get(chunk_index(x, z)).map(({ id }) => id),
+      })
+    }
+  })
 }
--- src/trade/trade.js
+++ src/trade/trade.js
@@ -2,60 +2,63 @@ import { version } from '../settings.js'
 import minecraftData from 'minecraft-data'
 
 const mcData = minecraftData(version)
+const { id: diamond } = mcData.findItemOrBlockByName('diamond')
+const { id: diamond_sword } = mcData.findItemOrBlockByName('diamond_sword')
 
-export function open_trade(world) {
-  const { villagers } = world
-  const { id: diamond } = mcData.findItemOrBlockByName('diamond')
-  const { id: diamond_sword } = mcData.findItemOrBlockByName('diamond_sword')
-
+export function register_trades(world) {
   const windowIds = new Map(
-    villagers.map((entityId, i) => [entityId, world.lastWindowId + i])
+    world.trade.villagers.map((entityId, i) => [
+      entityId,
+      world.lastWindowId + i,
+    ])
   )
-
-  function open_trade_handler({ client }) {
-    client.on('use_entity', ({ target, mouse, sneaking }) => {
-      if (villagers.includes(target) && mouse === 2 && sneaking === false) {
-        const windowId = windowIds.get(target)
-        const trade = {
-          windowId,
-          trades: [
-            {
-              inputItem1: {
-                present: true,
-                itemId: diamond,
-                itemCount: 1,
-              },
-              outputItem: {
-                present: true,
-                itemId: diamond_sword,
-                itemCount: 1,
-              },
-              inputItem2: {
-                present: true,
-                itemId: diamond,
-                itemCount: 1,
-              },
-              maximumNbTradeUses: 99999,
-            },
-          ],
-        }
-
-        const window = {
-          windowId,
-          inventoryType: 18, //todo : add all inventories in a file
-          windowTitle: JSON.stringify({ text: 'trade' }),
-        }
-
-        client.write('open_window', window)
-        client.write('trade_list', trade)
-      }
-    })
-  }
   return {
-    handlers: [open_trade_handler],
-    world: {
-      ...world,
-      lastWindowId: world.lastWindowId + windowIds.size,
+    ...world,
+    lastWindowId: world.lastWindowId + windowIds.size,
+    trade: {
+      ...world.trade,
+      windowIds,
     },
   }
+}
+
+export function open_trade({ client, world }) {
+  const { villagers, windowId } = world.trade
+  client.on('use_entity', ({ target, mouse, sneaking }) => {
+    if (villagers.includes(target) && mouse === 2 && sneaking === false) {
+      const windowId = windowIds.get(target)
+      const trade = {
+        windowId,
+        trades: [
+          {
+            inputItem1: {
+              present: true,
+              itemId: diamond,
+              itemCount: 1,
+            },
+            outputItem: {
+              present: true,
+              itemId: diamond_sword,
+              itemCount: 1,
+            },
+            inputItem2: {
+              present: true,
+              itemId: diamond,
+              itemCount: 1,
+            },
+            maximumNbTradeUses: 99999,
+          },
+        ],
+      }
+
+      const window = {
+        windowId,
+        inventoryType: 18, //todo : add all inventories in a file
+        windowTitle: JSON.stringify({ text: 'trade' }),
+      }
+
+      client.write('open_window', window)
+      client.write('trade_list', trade)
+    }
+  })
 }
