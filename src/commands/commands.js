0ed2e8331bd550d0960705ba4e320fd97f726768
Zekirax <tillsanta1034@gmail.com>
2020-12-27 15:10:12 +0100

    Commands

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -3,7 +3,7 @@ import { Position } from '../chat.js'
 import gamemode from './gamemode.js'
 
 export function write_unfounded_command({ sender }) {
-  const message = { translate: 'commands.help.failed' }
+  const message = { translate: 'commands.help.failed', color: 'red' }
   const options = {
     message: JSON.stringify(message),
     position: Position.CHAT,
@@ -17,19 +17,17 @@ export default function execute_command({ message, sender }) {
   const command = {
     command_name: splited_message[0],
     args: splited_message.slice(1),
+    sender,
   }
 
   switch (command.command_name) {
-    case '/gamemode':
-      gamemode({ args: command.args, sender })
-      break
-
     case '/gm':
-      gamemode({ args: command.args, sender })
+    case '/gamemode':
+      gamemode(command)
       break
 
     default:
-      write_unfounded_command({ sender })
+      write_unfounded_command(command)
       break
   }
 }
--- src/commands/gamemode.js
+++ src/commands/gamemode.js
@@ -27,7 +27,7 @@ const Succesful_Message = {
 }
 
 function write_error({ sender }) {
-  const message = { translate: 'command.failed' }
+  const message = { translate: 'arguments.operation.invalid', color: 'red' }
   const options = {
     message: JSON.stringify(message),
     position: Position.CHAT,
@@ -36,7 +36,7 @@ function write_error({ sender }) {
   sender.write('chat', options)
 }
 
-function write_gamemode({ sender, gameMode }, { message }) {
+function write_gamemode_and_message({ sender, gameMode }, { message }) {
   const gameMode_options = {
     reason: 3, // game_state_change is not only for gamemode but for a lot of things. And 3 is for gamemode.
     gameMode,
@@ -47,7 +47,7 @@ function write_gamemode({ sender, gameMode }, { message }) {
   const message_options = {
     message: JSON.stringify(message),
     position: Position.CHAT,
-    sender: sender.uuid,
+    sender: '00000000000000000000000000000000',
   }
 
   sender.write('chat', message_options)
@@ -56,50 +56,30 @@ function write_gamemode({ sender, gameMode }, { message }) {
 export default function gamemode({ args, sender }) {
   switch (args[0]) {
     case '0':
-      write_gamemode(
-        { sender, gameMode: GameMode.SURVIVAL },
-        { message: Succesful_Message.SURVIVAL }
-      )
-      break
     case 'survival':
-      write_gamemode(
+      write_gamemode_and_message(
         { sender, gameMode: GameMode.SURVIVAL },
         { message: Succesful_Message.SURVIVAL }
       )
       break
 
     case '1':
-      write_gamemode(
-        { sender, gameMode: GameMode.CREATIVE },
-        { message: Succesful_Message.CREATIVE }
-      )
-      break
     case 'creative':
-      write_gamemode(
+      write_gamemode_and_message(
         { sender, gameMode: GameMode.CREATIVE },
         { message: Succesful_Message.CREATIVE }
       )
       break
     case '2':
-      write_gamemode(
-        { sender, gameMode: GameMode.ADVENTURE },
-        { message: Succesful_Message.ADVENTURE }
-      )
-      break
     case 'adventure':
-      write_gamemode(
+      write_gamemode_and_message(
         { sender, gameMode: GameMode.ADVENTURE },
         { message: Succesful_Message.ADVENTURE }
       )
       break
     case '3':
-      write_gamemode(
-        { sender, gameMode: GameMode.SPECTATOR },
-        { message: Succesful_Message.SPECTATOR }
-      )
-      break
     case 'spectator':
-      write_gamemode(
+      write_gamemode_and_message(
         { sender, gameMode: GameMode.SPECTATOR },
         { message: Succesful_Message.SPECTATOR }
       )
