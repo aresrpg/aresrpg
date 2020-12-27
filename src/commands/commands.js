0569f18e4d83ad201b7f19d97f3c5536d4667313
Zekirax <tillsanta1034@gmail.com>
2020-12-27 22:14:21 +0100

    Commands

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -3,24 +3,25 @@ import { Position } from '../chat.js'
 import gamemode from './gamemode.js'
 
 export function write_unfounded_command({ sender }) {
-  const message = { translate: 'commands.help.failed', color: 'red' }
-  const options = {
-    message: JSON.stringify(message),
+  sender.write('chat', {
+    message: JSON.stringify({
+      translate: 'commands.help.failed',
+      color: 'red',
+    }),
     position: Position.CHAT,
     sender: sender.uuid,
-  }
-  sender.write('chat', options)
+  })
 }
 
 export default function execute_command({ message, sender }) {
-  const splited_message = message.trimStart().split(/\s+/)
+  const [name, ...args] = message.trimStart().split(/\s+/)
   const command = {
-    command_name: splited_message[0],
-    args: splited_message.slice(1),
+    name,
+    args,
     sender,
   }
 
-  switch (command.command_name) {
+  switch (command.name) {
     case '/gm':
     case '/gamemode':
       gamemode(command)
