11170e84e24dca3c1479cc21ebc150edd2ff526e
ToufouMaster <owen.kala@gmx.fr>
2021-01-02 00:04:11 +0100

    World Border
    Lowlife file Erased

--- src/lowlife.js
+++
@@ -1,8 +0,0 @@
-export function set_world_border({ client, radius }) {
-  client.write('world_border', { action: 0, radius })
-  client.write('world_border', { action: 2, x: 0, z: 0 })
-}
-
-export function red_screen({ client, warning_blocks }) {
-  client.write('world_border', { action: 5, warning_blocks })
-}
