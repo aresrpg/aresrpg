89f8580e754eb05462aede0f16af3c3248ab819f
ToufouMaster <owen.kala@gmx.fr>
2021-01-02 00:03:32 +0100

    World Border
    Added function to control World Border

---
+++ src/world_border.js
@@ -0,0 +1,22 @@
+export function set_world_border({ client, x, z, radius, speed }) {
+  client.write('world_border', {
+    action: 3,
+    x,
+    z,
+    old_radius: radius,
+    new_radius: radius,
+    speed,
+    portalBoundary: 29999984,
+    warning_time: 0,
+    warning_blocks: 0,
+  })
+}
+
+// Screen does'nt turn to red
+export function red_screen({ client }) {
+  client.write('world_border', { action: 5, warning_blocks: 5000 })
+}
+
+export function normal_screen({ client }) {
+  client.write('world_border', { action: 5, warning_blocks: 0 })
+}
