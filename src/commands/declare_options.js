abacb37c00f25345d69b62d9932710f2becb299d
Zekirax <tillsanta1034@gmail.com>
2021-02-28 16:56:49 +0100

    Add msg command

---
+++ src/commands/declare_options.js
@@ -0,0 +1,17 @@
+export const parser_properties = {
+  string: {
+    SINGLE_WORD: 0, // Reads a single word
+    QUOTABLE_PHRASE: 1, // If it starts with a ", keeps reading until another " (allowing escaping with \). Otherwise behaves the same as SINGLE_WORD
+    GREEDY_PHRASE: 2, // Reads the rest of the content after the cursor. Quotes will not be removed.
+  },
+  entity: {
+    ALL: 0x01, // If set, only allows a single entity/player
+    PLAYER: 0x02, // If set, only allows players
+  },
+}
+
+export const command_node_types = {
+  ROOT_INDEX: 0,
+  COMMAND: 1,
+  ARGUMENT: 2,
+}
