3a811e5f547f393bf749d230f91347e66c49a7eb
Duarte David <deltaduartedavid@gmail.com>
2021-03-21 15:18:29 +0100

    Cancel unhandled actions

--- src/player/inventory.js
+++ src/player/inventory.js
@@ -1,21 +1,77 @@
+import { on } from 'events'
+
+import { aiter } from 'iterator-helper'
+
+import { empty_slot, item_to_slot } from '../items.js'
+import { PLAYER_INVENTORY_ID } from '../index.js'
+
 export default {
   /** @type {import('../index.js').Reducer} */
   reduce(state, { type, payload }) {
     if (type === 'packet/window_click') {
-      const { windowId, slot } = payload
-      if (windowId !== 0) return state
-      const _state = {
-        ...state,
-        inventory: [
-          ...state.inventory.slice(0, slot),
-          state.cursor_item_selected,
-          ...state.inventory.slice(slot + 1),
-        ],
-        cursor_item_selected: state.inventory[slot],
+      const { windowId, slot, mode } = payload
+
+      if (windowId === PLAYER_INVENTORY_ID) {
+        if (mode === 0) {
+          const drop = slot === -999
+
+          const index = drop ? state.inventory_cursor_index : slot
+
+          // Put cursor at index
+          const inventory = [
+            ...state.inventory.slice(0, index),
+            state.inventory_cursor,
+            ...state.inventory.slice(index + 1),
+          ]
+
+          return {
+            ...state,
+            inventory,
+            // Resync if we drop
+            inventory_sequence_number:
+              state.inventory_sequence_number + (drop ? 1 : 0),
+            inventory_cursor: state.inventory[index],
+            inventory_cursor_index:
+              state.inventory_cursor == null
+                ? index
+                : state.inventory_cursor_index,
+          }
+        } else {
+          /* Unhandled action resync inventory */
+          return {
+            ...state,
+            inventory_sequence_number: state.inventory_sequence_number + 1,
+          }
+        }
       }
-      console.log(_state.inventory, _state.cursor_item_selected)
-      return _state
     }
     return state
+  },
+  /** @type {import('../index.js').Observer} */
+  observe({ client, events, world }) {
+    aiter(on(events, 'state')).reduce(
+      (
+        last_sequence_number,
+        [{ inventory, inventory_cursor, inventory_sequence_number }]
+      ) => {
+        if (last_sequence_number !== inventory_sequence_number) {
+          const to_slot = (item) =>
+            item ? item_to_slot(world.items[item.type], item.count) : empty_slot
+
+          client.write('window_items', {
+            windowId: PLAYER_INVENTORY_ID,
+            items: inventory.map(to_slot),
+          })
+
+          client.write('set_slot', {
+            windowId: -1,
+            slot: -1,
+            item: to_slot(inventory_cursor),
+          })
+        }
+        return inventory_sequence_number
+      },
+      null
+    )
   },
 }
