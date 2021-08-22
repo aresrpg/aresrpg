22b9b352b2cbc3db5ed51b0141237795cc7e0f7f
sceat <fetch@sceat.xyz>
2021-08-22 23:17:06 +0200

    listen held_item_slot packet & update inventory

--- src/player/held_item.js
+++ src/player/held_item.js
@@ -5,16 +5,25 @@ const hotbar_to_inventory_slot = (slot_id) => slot_id + 36
 export default {
   /** @type {import('../index.js').Reducer} */
   reduce(state, { type, payload }) {
+    if(type === 'packet/held_item_slot') {
+      const { slotId } = payload
+      return {
+        ...state,
+        held_slot_index: slotId
+      }
+    }
     if (type === 'packet/block_dig' && payload.status === SWAP_HAND_STATUS) {
-      const held_item_slot = hotbar_to_inventory_slot(state.held_slot_index)
+      const { held_slot_index } = state
+      const inventory_slot = hotbar_to_inventory_slot(held_slot_index)
       const inventory = [...state.inventory]
-      ;[inventory[OFF_HAND_SLOT], inventory[held_item_slot]] = [
-        inventory[held_item_slot],
+      ;[inventory[OFF_HAND_SLOT], inventory[inventory_slot]] = [
+        inventory[inventory_slot],
         inventory[OFF_HAND_SLOT],
       ]
       return {
         ...state,
         inventory,
+        inventory_sequence_number: state.inventory_sequence_number + 1,
       }
     }
     return state
