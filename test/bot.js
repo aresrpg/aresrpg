166b84e8d336f91f53841e714980efbd30a78cd8
Valentin <nexomega.minecraft@gmail.com>
2021-12-08 17:53:08 +0100

    Working mineflayer and doubt

--- test/bot.js
+++ test/bot.js
@@ -1,17 +1,7 @@
-// import '../src/index.js'
 import mineflayer from 'mineflayer'
-import Doubt from '@hydre/doubt'
 
-export const bot = mineflayer.createBot({
+export default mineflayer.createBot({
   username: '#iRobot',
   host: '127.0.0.1',
   port: '25567',
-})
-
-// bot.on('kicked', console.log)
-
-export const doubt = Doubt({
-  title: 'Testing client',
-  calls: 1,
-  stdout: process.stdout,
 })
--- test/test_runner.js
+++ test/test_runner.js
@@ -1,1 +1,22 @@
-import './connected.js'
+import Doubt from '@hydre/doubt'
+
+import '../src/index.js'
+import logger from '../src/logger.js'
+
+import connected from './connected.doubt.js'
+
+export const doubt = Doubt({
+  title: 'Testing client',
+  calls: 1,
+  stdout: process.stdout,
+})
+
+export const log = logger(import.meta)
+
+const to_test = new Set()
+
+to_test.add(connected)
+
+for (const test of to_test) {
+  test.test()
+}
