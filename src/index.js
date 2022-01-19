7071ee34f6a17c2639f4e97bcaeb1fc6df6c17b8
Sceat <fetch@sceat.xyz>
2022-01-20 01:17:57 +0200

    feat: add MOTD support

--- src/index.js
+++ src/index.js
@@ -5,19 +5,40 @@ import { fileURLToPath } from 'url'
 import pino from 'pino'
 import protocol from 'minecraft-protocol'
 
+import motd_component from './motd.js'
 import { ONLINE_MODE, VERSION } from './settings.js'
 import logger from './logger.js'
 
 const log = logger(import.meta)
+const MAX_PLAYERS = -1
+const favicon = `data:image/png;base64,${fs.readFileSync(
+  join(dirname(fileURLToPath(import.meta.url)), '../media/favicon.png'),
+  'base64'
+)}`
 
 const server = protocol.createServer({
   version: VERSION,
   'online-mode': ONLINE_MODE,
   motd: 'loading...',
-  favicon: `data:image/png;base64,${fs.readFileSync(
-    join(dirname(fileURLToPath(import.meta.url)), '../media/favicon.png'),
-    'base64'
-  )}`,
+  maxPlayers: MAX_PLAYERS,
+  favicon,
+  beforePing: (response, client) => {
+    return {
+      ...response,
+      players: {
+        max: MAX_PLAYERS,
+        online: response.players.online,
+        sample: [
+          {
+            name: 'Built on Solana',
+            id: '00000000-0bad-cafe-babe-000000000000',
+          },
+        ],
+      },
+      description: motd_component,
+      favicon,
+    }
+  },
 })
 
 const context = import('./context.js')
