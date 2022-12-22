c7f8b729c75c3c34a5600676e1b8aecd29217b20
Sceat <fetch@sceat.xyz>
2022-12-22 21:45:08 +0700

    fix: prevent unknown items crash

--- src/player/inventory.js
+++ src/player/inventory.js
@@ -7,6 +7,9 @@ import { PLAYER_INVENTORY_ID } from '../settings.js'
 import { abortable } from '../iterator.js'
 import { Action, Context } from '../events.js'
 import items from '../../data/items.json' assert { type: 'json' }
+import logger from '../logger.js'
+
+const log = logger(import.meta)
 
 const FORBIDDEN_SLOTS = [
   0, // Craft Output
@@ -19,8 +22,16 @@ const FORBIDDEN_SLOTS = [
 export const USABLE_INVENTORY_START = 9
 export const USABLE_INVENTORY_END = 44
 
-const to_slot = item =>
-  item ? item_to_slot(items[item.type], item.count) : empty_slot
+const to_slot = item => {
+  const item_definition = items[item?.type]
+  if (!item_definition) {
+    // in case the item is provided but no match was found in item.js
+    // we log the problem
+    if (item) log.warn(item, 'Tried to spawn an unexisting item, ignoring..')
+    return empty_slot
+  }
+  return item_to_slot(item_definition, item.count)
+}
 
 const BlockDigStatus = {
   STARTED_DIGGING: 0,
