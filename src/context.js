7242802796eff2cc40f73a535699284b2cb077b8
Sceat <fetch@sceat.xyz>
2022-12-22 21:45:08 +0700

    fix: prevent unknown items crash

--- src/context.js
+++ src/context.js
@@ -110,9 +110,6 @@ const initial_state = {
   view_distance: 0,
   inventory: Array.from({
     length: 46,
-    36: { type: 'spellbook', count: 1 },
-    37: { type: 'bronze_coin', count: 10 },
-    38: { type: 'menitrass_100', count: 1 },
   }),
   looted_items: {
     pool: Array.from({ length: ITEM_LOOT_MAX_COUNT }),
