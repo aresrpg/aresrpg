6d9dd7aecf94d5e92511fc8f29b7529406d87d92
KalvinVilla <villa.kalvin@gmail.com>
2022-12-28 09:04:51 +0100

    add sohkey code, need to do some changement

--- src/player/interactable_object.js
+++ src/player/interactable_object.js
@@ -1,11 +1,36 @@
+import equal from 'fast-deep-equal'
+
 import { client_chat_msg } from '../chat.js'
 import { get_block } from '../chunk.js'
 
-const DOOR_TYPE = 161
+const interactable_types = {
+  trap: 222,
+  door: 161,
+}
 
-const PERMIT = {
-  ALLOW: 'allow',
-  DENY: 'deny',
+const door_state = {
+  upper: 3576,
+  bottom: 3584,
+}
+
+function update_door(position) {
+  const { x, y, z } = position
+  const upper_location = {
+    x,
+    y: y + 1,
+    z,
+  }
+
+  const upper = {
+    location: upper_location,
+    type: door_state.upper,
+  }
+  const bottom = {
+    location: position,
+    type: door_state.bottom,
+  }
+
+  return [upper, bottom]
 }
 
 /**
@@ -23,69 +48,50 @@ export default {
     client.on('block_place', async ({ location }) => {
       const block = await get_block(world, location)
 
-      /*
-       * Find if the block is on the interact block list
-       * Can't use const beacuse door is not the same condition
-       */
-      let interact_object
+      const handle_interactable = async ({
+        allow,
+        position,
+        message,
+        type,
+      }) => {
+        if (!allow) {
+          client_chat_msg({ client, message })
 
-      // TAG is used for know if the client click on the toppom or bottom of the door
-      let tag = 0
-
-      /**
-       * If the block is the door we need to show if the bottom block is on the interactale object list
-       */
-      if (block.type === DOOR_TYPE) {
-        interact_object = world.interactable_object.find(
-          ({ type, location: loc }) =>
-            type === block.type &&
-            ((location.x === loc.x &&
-              (location.y === loc.y || location.y - 1 === loc.y) &&
-              location.z === loc.z) ||
-              loc === '*')
-        )
-
-        if (
-          interact_object !== undefined &&
-          interact_object.location.y !== location.y
-        )
-          tag = 1
-      } else {
-        interact_object = world.interactable_object.find(
-          ({ type, location: loc }) =>
-            type === block.type &&
-            ((location.x === loc.x &&
-              location.y === loc.y &&
-              location.z === loc.z) ||
-              loc === '*')
-        )
-      }
-
-      if (interact_object !== undefined) {
-        if (interact_object.permit === PERMIT.DENY) {
-          /* CANCEL ACTION */
-          client.write('block_change', {
-            location,
-            type: block.stateId,
-          })
-
-          if (interact_object.custom_message !== undefined)
-            client_chat_msg({ client, message: interact_object.custom_message })
-
-          if (block.type === DOOR_TYPE) {
-            const locblock2 = {
-              x: location.x,
-              z: location.z,
-              y: tag === 0 ? location.y + 1 : location.y - 1,
-            }
-            const block2 = await get_block(world, locblock2)
+          if (type === interactable_types.door) {
+            update_door(position).forEach(block => {
+              client.write('block_change', block)
+            })
+          } else {
             client.write('block_change', {
-              location: locblock2,
-              type: block2.stateId,
+              location: position,
+              type: block.stateId,
             })
           }
         }
       }
+
+      const interacted_object = world.interactable_object.find(payload => {
+        const { position } = payload
+
+        if (block.type === interactable_types.door) {
+          // check if the upper block is interacted
+          const { x, y, z } = position
+          const upper_door = {
+            x,
+            y: y + 1,
+            z,
+          }
+
+          if (equal(upper_door, location)) return payload
+        }
+
+        if (equal(position, location)) return payload
+
+        // NEED TO REMOVE
+        return payload
+      })
+
+      if (interacted_object) handle_interactable(interacted_object)
     })
   },
 }
