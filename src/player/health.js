cec32f896107bb6817fe1e18eeedc70c72609387
Sceat <fetch@sceat.xyz>
2023-02-27 01:17:30 +0800

    feat: implement health regeneration

--- src/player/health.js
+++ src/player/health.js
@@ -1,20 +1,25 @@
 import { on } from 'events'
+import { setInterval } from 'timers/promises'
 
 import { aiter } from 'iterator-helper'
 
-import { get_max_health } from '../characteristics.js'
+import {
+  Characteristic,
+  get_max_health,
+  get_total_characteristic,
+} from '../characteristics.js'
+import { compute_damage } from '../damage.js'
 import { PlayerEvent, WorldRequest } from '../events.js'
 import { abortable } from '../iterator.js'
-import logger from '../logger.js'
+import { normalize_range } from '../math.js'
 import { play_sound } from '../sound.js'
 import { synchronisation_payload } from '../sync.js'
-
-const log = logger(import.meta)
 
 export const SCOREBOARD_NAME = 'life'
 const CREATE_OBJECTIVE_ACTION = 0
 const INTEGER_TYPE = 0
 const BELOW_NAME_POSITION = 2
+const REGENERATION_MIND_DIVIDER = 50
 
 export default {
   /** @type {import('../context.js').Reducer} */
@@ -22,8 +27,6 @@ export default {
     if (type === PlayerEvent.UPDATE_HEALTH) {
       const max_health = get_max_health(state)
       const health = Math.max(0, Math.min(max_health, payload.health))
-
-      log.info({ health }, 'direct health update')
 
       return {
         ...state,
@@ -34,7 +37,7 @@ export default {
   },
 
   /** @type {import('../context.js').Observer} */
-  observe({ client, events, signal, dispatch, world }) {
+  observe({ client, events, signal, dispatch, world, get_state }) {
     events.once(PlayerEvent.STATE_UPDATED, state => {
       client.write('scoreboard_objective', {
         name: SCOREBOARD_NAME,
@@ -49,13 +52,35 @@ export default {
       })
     })
 
+    aiter(abortable(setInterval(1000, { signal })))
+      .map(get_state)
+      .filter(state => !!state)
+      .forEach(({ health, inventory, characteristics }) => {
+        const mind = get_total_characteristic(Characteristic.MIND, {
+          inventory,
+          characteristics,
+        })
+        const regeneration_per_second = compute_damage({
+          base_damage: mind / REGENERATION_MIND_DIVIDER,
+          characteristic_amount: mind,
+        })
+        if (health > 0)
+          dispatch(PlayerEvent.UPDATE_HEALTH, {
+            health: health + regeneration_per_second,
+          })
+      })
+
     aiter(abortable(on(events, PlayerEvent.STATE_UPDATED, { signal })))
       .map(([state]) => state)
       .reduce((last_health, state) => {
         const { position, health } = state
-        if (last_health !== null && last_health !== health) {
+        if (last_health !== health) {
           client.write('update_health', {
-            health,
+            health: normalize_range(
+              health,
+              { min: 0, max: get_max_health(state) },
+              { min: 0, max: 40 }
+            ),
             food: 20,
             foodSaturation: 0.0,
           })
@@ -74,11 +99,12 @@ export default {
             })
 
           if (health === 0) {
-            dispatch(PlayerEvent.TELEPORT_TO, world.spawn_position)
             dispatch(PlayerEvent.DIE)
             setTimeout(() => {
               // delaying to let the time of the death animation
               world.events.emit(WorldRequest.PLAYER_DIED, { uuid: client.uuid })
+              // allows a correct handling of the position
+              dispatch(PlayerEvent.TELEPORT_TO, world.spawn_position)
             }, 700)
             play_sound({
               client,
